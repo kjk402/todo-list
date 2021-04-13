521d540bb0edc7f3fa617e44b34c5d426ce26dff
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-04-13 22:40:32 +0900

    fix: Fix query

--- backend/src/main/java/com/codesquad/todolist/domain/Card.java
+++ backend/src/main/java/com/codesquad/todolist/domain/Card.java
@@ -1,6 +1,7 @@
 package com.codesquad.todolist.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
